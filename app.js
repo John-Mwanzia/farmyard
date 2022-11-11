8e91bc53f6c18b590551c29b824e0e3314ab1c0a
John mwanzia <johnmwanzia277@gmail.com>
2022-11-10 23:23:20 -0800

    send an email when client submits the form

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public" ))
 app.get("/", function(req, res){
     res.sendFile((__dirname + "/index.html"));
@@ -9,10 +13,36 @@ app.post("/", function(req, res){
     const fName = req.body.firstname
     const lName = req.body.lastname
     const email = req.body.email
-    const county = req.body.county
-    const message= req.body.subject
+    const subject = req.body.subject
+    const message= req.body.message
 
-    console.log(fName);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASSWORD
+        }
+      });
+      
+      var mailOptions = {
+        from: email,
+        to: 'jmwanzia@kabarak.ac.ke',
+        subject: subject,
+        text: message
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.send("hello" + " " + fName + " " + lName +  " " + "Your request submitted successfuly")
+        }
+      });
+
+  
+
 })
 
 
